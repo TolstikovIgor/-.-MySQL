01b925997c9bfb6fed0f0fc2fffe4664c378ceda
TolstikovIgor <76521844+TolstikovIgor@users.noreply.github.com>
2022-02-14 16:45:51 +0300

    Create task4.sql

---
+++ lesson02/task4.sql
@@ -0,0 +1,4 @@
+/*Ознакомьтесь более подробно с документацией утилиты mysqldump. Создайте дамп единственной таблицы help_keyword базы данных mysql. Причем добейтесь того, чтобы дамп содержал только первые 100 строк таблицы.
+*/
+
+mysqldump mysql help_keyword --where='1 ORDER BY help_keyword_id LIMIT 100' > help_keyword_report.sql
